6a1bff1376e09ba2c67879693de11aeaddf7dd46
ITISH <itishjain7@gmail.com>
2024-03-31 10:17:29 +0000

    added some code

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+  
+
+
+
+
+
+mongoose.connect("mongodb+srv://itish_jain:12345@cluster0.vxfie1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+{
+    dbName:"frappi"
+
+}).then(()=>{
+    console.log("database connected succesfully")
+}).catch(err=>{
+    console.log("error in connecting".err)
+})
+
+
+
+
+
+
 
 
 
@@ -7,7 +30,7 @@ app.get("/",(req,res)=>{
     res.send("hello moto");
 })
 
-
+// data base ki connectivity 
 app.listen(4040,()=>{
     let a=10;
     console.log(a);
